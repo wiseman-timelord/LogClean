48e763fd7dd01c0dec07c449fe01a1b005ca0916
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-01-09 00:28:25 +0000

    It works....

---
+++ logclean-ps.ps1
@@ -0,0 +1,31 @@
+# Script: LogClean-Ps
+
+
+Write-Host "Operations Starting..."
+
+# Searching for the first .log file in the current directory
+$logFiles = Get-ChildItem -Filter *.log
+if ($logFiles.Count -eq 0) {
+    Write-Host "No .log files found in the current directory."
+    exit
+}
+$filePath = $logFiles[0].FullName
+
+Write-Host "Reading $filePath"
+Start-Sleep -Seconds 1
+$originalContent = Get-Content $filePath
+
+Write-Host "Removing ANSI codes..."
+Start-Sleep -Seconds 1
+$regex = '\u001b\[[0-9;]*[mK]'  # Updated regex for matching ANSI codes
+$filteredContent = $originalContent | Foreach-Object { $_ -replace $regex, '' }
+
+$filteredFilePath = "$filePath"
+Write-Host "Writing $filteredFilePath"
+Start-Sleep -Seconds 1
+$filteredContent | Set-Content $filteredFilePath
+
+# Exit Script
+Write-Host "...Operations completed."
+Write-Host
+Start-Sleep -Seconds 3
