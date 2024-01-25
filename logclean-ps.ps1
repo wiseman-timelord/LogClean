df6a3175558ac18632f0ae3ef038c1fcbfbb1723
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-01-25 13:43:43 +0000

    Revisited

--- logclean-ps.ps1
+++ logclean-ps.ps1
@@ -1,31 +1,30 @@
-# Script: LogClean-Ps
+# Corrected Script
 
 Write-Host "Operations Starting..."
 
-# Searching for the first .log file in the current directory
+# Finding Log
 $logFiles = Get-ChildItem -Filter *.log
 if ($logFiles.Count -eq 0) {
     Write-Host "No .log files found in the current directory."
     exit
 }
 $filePath = $logFiles[0].FullName
-$fileName = $logFiles[0].Name  # Extracts just the filename
+$fileName = $logFiles[0].Name
 
+# Cleaning Log
 Write-Host "Reading: $fileName"
 Start-Sleep -Seconds 1
 $originalContent = Get-Content $filePath
-
-Write-Host "Removing ANSI codes.."
+Write-Host "Removing ANSI codes..="
 Start-Sleep -Seconds 1
-$regex = '\u001b\[[0-9;]*[mK]'  # Updated regex for matching ANSI codes
+$regex = '\u001b\[[0-9;]*[mK]'
 $filteredContent = $originalContent | Foreach-Object { $_ -replace $regex, '' }
-
 $filteredFilePath = "$filePath"
 Write-Host "Writing: $fileName"
 Start-Sleep -Seconds 1
 $filteredContent | Set-Content $filteredFilePath
 
 # Exit Script
-Write-Host "...Operations completed."
+Write-Host "...=Operations completed."
 Write-Host
 Start-Sleep -Seconds 1
